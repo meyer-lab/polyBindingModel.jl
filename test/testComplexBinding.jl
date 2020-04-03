830a3e4146081752f169daf4c6b4bb2514b808b9
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2020-04-03 11:24:39 -0700

    Try this. (#1)
    Split test group to print output more frequently.

--- test/testComplexBinding.jl
+++ test/testComplexBinding.jl
@@ -23,58 +23,56 @@ function polyfc_via_polyc(L0::Real, KxStar::Real, f::Number, Rtot::Vector, LigC:
 end
 
 
-@testset "complexBinding.jl tests" begin
-    @testset "Give the same results as fcBindingModel" begin
-        for i = 1:10
-            L0 = rand() * 10.0^rand(-15:-5)
-            KxStar = rand() * 10.0^rand(-15:-5)
-            f = rand(2:20)
-            nl = rand(1:10)
-            nr = rand(1:10)
-            Rtot = floor.(100 .+ rand(nr) .* (10 .^ rand(4:6, nr)))
-            LigC = rand(nl) .* (10 .^ rand(1:2, nl))
-            Kav = rand(nl, nr) .* (10 .^ rand(3:7, nl, nr))
+@testset "Give the same results as fcBindingModel" begin
+    for i = 1:10
+        L0 = rand() * 10.0^rand(-15:-5)
+        KxStar = rand() * 10.0^rand(-15:-5)
+        f = rand(2:20)
+        nl = rand(1:10)
+        nr = rand(1:10)
+        Rtot = floor.(100 .+ rand(nr) .* (10 .^ rand(4:6, nr)))
+        LigC = rand(nl) .* (10 .^ rand(1:2, nl))
+        Kav = rand(nl, nr) .* (10 .^ rand(3:7, nl, nr))
 
-            old_res = polyfc(L0, KxStar, f, Rtot, LigC, Kav)
-            new_res = polyfc_via_polyc(L0, KxStar, f, Rtot, LigC, Kav)
-            @test old_res.Lbound ≈ new_res[1]
-            @test old_res.Rbound ≈ new_res[2]
-        end
+        old_res = polyfc(L0, KxStar, f, Rtot, LigC, Kav)
+        new_res = polyfc_via_polyc(L0, KxStar, f, Rtot, LigC, Kav)
+        @test old_res.Lbound ≈ new_res[1]
+        @test old_res.Rbound ≈ new_res[2]
     end
+end
 
-    @testset "complexBinding can take ForwardDiff" begin
-        L0 = 1.0e-8
-        KxStar = 1.2e-10
-        Rtot = [100.0, 1000.0, 10.0, 10000.0]
-        Cplx = [1 0 3; 2 2 0; 1 1 2; 4 0 0]
-        Ctheta = rand(4)
-        Ctheta = Ctheta / sum(Ctheta)
-        Kav = rand(3, 4) * 1.0e7
-        Ltheta = L0 .* Ctheta
+@testset "complexBinding can take ForwardDiff" begin
+    L0 = 1.0e-8
+    KxStar = 1.2e-10
+    Rtot = [100.0, 1000.0, 10.0, 10000.0]
+    Cplx = [1 0 3; 2 2 0; 1 1 2; 4 0 0]
+    Ctheta = rand(4)
+    Ctheta = Ctheta / sum(Ctheta)
+    Kav = rand(3, 4) * 1.0e7
+    Ltheta = L0 .* Ctheta
 
-        for i = 1:2
-            func = x -> polyc(x, KxStar, Rtot, Cplx, Ctheta, Kav)[i]
-            out = ForwardDiff.derivative(func, L0)
-            @test typeof(out) == Float64
+    for i = 1:2
+        func = x -> polyc(x, KxStar, Rtot, Cplx, Ctheta, Kav)[i]
+        out = ForwardDiff.derivative(func, L0)
+        @test typeof(out) == Float64
 
-            func = x -> polyc(L0, x, Rtot, Cplx, Ctheta, Kav)[i]
-            out = ForwardDiff.derivative(func, KxStar)
-            @test typeof(out) == Float64
+        func = x -> polyc(L0, x, Rtot, Cplx, Ctheta, Kav)[i]
+        out = ForwardDiff.derivative(func, KxStar)
+        @test typeof(out) == Float64
 
-            func = x -> polyc(L0, KxStar, x, Cplx, Ctheta, Kav)[i]
-            out = ForwardDiff.gradient(func, Rtot)
-            @test eltype(out) == Float64
-            @test length(out) == length(Rtot)
+        func = x -> polyc(L0, KxStar, x, Cplx, Ctheta, Kav)[i]
+        out = ForwardDiff.gradient(func, Rtot)
+        @test eltype(out) == Float64
+        @test length(out) == length(Rtot)
 
-            func = x -> polyc(L0, KxStar, Rtot, Cplx, x, Kav)[i]
-            out = ForwardDiff.gradient(func, Ctheta)
-            @test eltype(out) == Float64
-            @test length(out) == length(Ctheta)
+        func = x -> polyc(L0, KxStar, Rtot, Cplx, x, Kav)[i]
+        out = ForwardDiff.gradient(func, Ctheta)
+        @test eltype(out) == Float64
+        @test length(out) == length(Ctheta)
 
-            func = x -> polycm(KxStar, Rtot, Cplx, x, Kav)[i]
-            out = ForwardDiff.gradient(func, Ltheta)
-            @test eltype(out) == Float64
-            @test length(out) == length(Ltheta)
-        end
+        func = x -> polycm(KxStar, Rtot, Cplx, x, Kav)[i]
+        out = ForwardDiff.gradient(func, Ltheta)
+        @test eltype(out) == Float64
+        @test length(out) == length(Ltheta)
     end
 end
