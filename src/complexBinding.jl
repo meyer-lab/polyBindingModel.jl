6a87c1e7a6c82e8cea1e24783d153a5df506044d
Aaron Meyer <ameyer@ucla.edu>
2021-02-14 07:00:03 -0800

    More concise

--- src/complexBinding.jl
+++ src/complexBinding.jl
@@ -8,29 +8,29 @@ function Req_func(Req::Vector, L0::Real, Kx::Real, Rtot::Vector, Cplx::AbstractM
 end
 
 
-function polyc(L0::Real, KxStar::Real, Rtot::Vector, Cplx::AbstractMatrix, θ::Vector, Kav::Matrix)
+function polyc(L0::Real, Kₓ::Real, Rtot::Vector, Cplx::AbstractMatrix, θ::Vector, Kav::Matrix)
     ncplx = size(Cplx, 1)
     @assert size(Cplx, 2) == size(Kav, 1)
     @assert ncplx == length(θ)
     @assert size(Kav, 2) == length(Rtot)
     θ /= sum(θ)
 
-    ansType = promote_type(typeof(L0), typeof(KxStar), eltype(Rtot), eltype(θ))
-    f! = (F, x) -> F .= Req_func(x, L0, KxStar, Rtot, Cplx, θ, Kav)
+    ansType = promote_type(typeof(L0), typeof(Kₓ), eltype(Rtot), eltype(θ))
+    f! = (F, x) -> F .= Req_func(x, L0, Kₓ, Rtot, Cplx, θ, Kav)
     Req = rootSolve(f!, convert(Vector{ansType}, Rtot))
 
-    Psi = Kav .* transpose(Req) .* KxStar
+    Psi = Kav .* transpose(Req) .* Kₓ
     PsiRS = sum(Psi, dims = 2) .+ 1.0
-    Lbounds = L0 / KxStar * θ .* expm1.(Cplx * log1p.(PsiRS .- 1))
-    Rbounds = L0 / KxStar * θ .* Cplx * (Psi ./ PsiRS) .* exp.(Cplx * log1p.(PsiRS .- 1))
-    Lfbnds = L0 / KxStar * θ .* exp.(Cplx * log.(PsiRS .- 1.0))
-    Lmbnds = L0 / KxStar * θ .* Cplx * (PsiRS .- 1)
+    Lbounds = L0 / Kₓ * θ .* expm1.(Cplx * log1p.(PsiRS .- 1))
+    Rbounds = L0 / Kₓ * θ .* Cplx * (Psi ./ PsiRS) .* exp.(Cplx * log1p.(PsiRS .- 1))
+    Lfbnds = L0 / Kₓ * θ .* exp.(Cplx * log.(PsiRS .- 1.0))
+    Lmbnds = L0 / Kₓ * θ .* Cplx * (PsiRS .- 1)
 
-    @assert sum(Rbounds) ≈ L0 / KxStar * sum(θ .* (Cplx * (1 .- 1 ./ PsiRS)) .* exp.(Cplx * log.(PsiRS)))
+    @assert sum(Rbounds) ≈ L0 / Kₓ * sum(θ .* (Cplx * (1 .- 1 ./ PsiRS)) .* exp.(Cplx * log.(PsiRS)))
     @assert length(Lbounds) == ncplx
     @assert size(Rbounds, 1) == length(θ)
     @assert size(Rbounds, 2) == length(Rtot)
     return Lbounds, Rbounds, Lfbnds, Lmbnds
 end
 
-polycm = (KxStar, Rtot, Cplx, Ltheta, Kav) -> polyc(sum(Ltheta), KxStar, Rtot, Cplx, Ltheta ./ sum(Ltheta), Kav)
+polycm = (Kₓ, Rtot, Cplx, Ltheta, Kav) -> polyc(sum(Ltheta), Kₓ, Rtot, Cplx, Ltheta ./ sum(Ltheta), Kav)
--- src/randomBinding.jl
+++ src/randomBinding.jl
@@ -9,35 +9,35 @@ mutable struct fcOutput{T}
 end
 
 
-function polyfc(L0::Real, KxStar::Real, f::Number, Rtot::Vector, IgGC::Vector, Kav::AbstractMatrix)
+function polyfc(L0::Real, Kₓ::Real, f::Number, Rtot::Vector, IgGC::Vector, Kav::AbstractMatrix)
     # Data consistency check
     (ni, nr) = size(Kav)
     @assert ni == length(IgGC)
     @assert nr == length(Rtot)
     IgGC /= sum(IgGC)
 
-    ansType = promote_type(typeof(L0), typeof(KxStar), typeof(f), eltype(Rtot), eltype(IgGC))
+    ansType = promote_type(typeof(L0), typeof(Kₓ), typeof(f), eltype(Rtot), eltype(IgGC))
 
-    Av = transpose(Kav) * IgGC * KxStar
-    f! = (F, x) -> F .= x + L0 * f / KxStar .* (x .* Av) * (1 + sum(x .* Av))^(f - 1) - Rtot
+    Av = transpose(Kav) * IgGC * Kₓ
+    f! = (F, x) -> F .= x + L0 * f / Kₓ .* (x .* Av) * (1 + sum(x .* Av))^(f - 1) - Rtot
 
     Req = rootSolve(f!, convert(Vector{ansType}, Rtot))
 
-    ansType = promote_type(typeof(L0), typeof(KxStar), typeof(f), eltype(Rtot), eltype(IgGC))
+    ansType = promote_type(typeof(L0), typeof(Kₓ), typeof(f), eltype(Rtot), eltype(IgGC))
     Phi = ones(ansType, ni, nr + 1) .* IgGC
-    Phi[:, 1:nr] .*= Kav .* transpose(Req) .* KxStar
+    Phi[:, 1:nr] .*= Kav .* transpose(Req) .* Kₓ
     Phisum = sum(Phi[:, 1:nr])
     Phisum_n = sum(Phi[:, 1:nr], dims = 1)
 
     w = fcOutput{ansType}(
-        L0 / KxStar * ((1 + Phisum)^f - 1),
-        L0 / KxStar * f * Phisum * (1 + Phisum)^(f - 1),
-        L0 / KxStar * f * Phisum * ((1 + Phisum)^(f - 1) - 1),
+        L0 / Kₓ * ((1 + Phisum)^f - 1),
+        L0 / Kₓ * f * Phisum * (1 + Phisum)^(f - 1),
+        L0 / Kₓ * f * Phisum * ((1 + Phisum)^(f - 1) - 1),
         Req,
-        vec(L0 / KxStar * f .* Phisum_n * (1 + Phisum)^(f - 1)),
-        vec(L0 / KxStar * f .* Phisum_n * ((1 + Phisum)^(f - 1) - 1)),
+        vec(L0 / Kₓ * f .* Phisum_n * (1 + Phisum)^(f - 1)),
+        vec(L0 / Kₓ * f .* Phisum_n * ((1 + Phisum)^(f - 1) - 1)),
     )
     return w
 end
 
-polyfcm = (KxStar, f, Rtot, IgG, Kav) -> polyfc(sum(IgG) / f, KxStar, f, Rtot, IgG ./ sum(IgG), Kav)
+polyfcm = (Kₓ, f, Rtot, IgG, Kav) -> polyfc(sum(IgG) / f, Kₓ, f, Rtot, IgG ./ sum(IgG), Kav)
