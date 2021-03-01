3221f9922c906d0ef8ffb4e2374108691cce3752
Aaron Meyer <ameyer@ucla.edu>
2021-03-01 08:28:10 -0800

    More edit

--- src/randomBinding.jl
+++ src/randomBinding.jl
@@ -9,21 +9,15 @@ mutable struct fcOutput{T}
 end
 
 
-function polyfc(L0::Real, Kₓ::Real, f::Number, Rtot::Vector, IgGC::Vector, Kav::AbstractMatrix)
+function polyfc_Req(Req::Vector, L0::Real, Kₓ::Real, f::Number, Rtot::Vector, IgGC::Vector, Kav::AbstractMatrix)
     # Data consistency check
     @assert size(Kav, 1) == length(IgGC)
     @assert size(Kav, 2) == length(Rtot)
+    ansType = promote_type(typeof(L0), typeof(Kₓ), typeof(f), eltype(Rtot), eltype(IgGC), eltype(Req))
+
     IgGC /= sum(IgGC)
-
-    ansType = promote_type(typeof(L0), typeof(Kₓ), typeof(f), eltype(Rtot), eltype(IgGC))
-
-    Av = transpose(Kav) * IgGC * Kₓ
     L0fK = L0 * f / Kₓ
-    f! = (F, x) -> F .= x + L0fK .* (x .* Av) * (1 + x' * Av)^(f - 1) - Rtot
 
-    Req = rootSolve(f!, convert(Vector{ansType}, Rtot))
-
-    ansType = promote_type(typeof(L0), typeof(Kₓ), typeof(f), eltype(Rtot), eltype(IgGC))
     Phi = ones(ansType, size(Kav, 1), size(Kav, 2) + 1) .* IgGC
     Phi[:, 1:size(Kav, 2)] .*= Kav .* transpose(Req) .* Kₓ
     Phisum_n = sum(Phi[:, 1:size(Kav, 2)], dims = 1)
@@ -34,10 +28,19 @@ function polyfc(L0::Real, Kₓ::Real, f::Number, Rtot::Vector, IgGC::Vector, Kav
         sum(Rtot - Req),
         L0fK * Phisum * ((1 + Phisum)^(f - 1) - 1),
         Req,
-        Rtot - Req,
+        vec(L0 / Kₓ * f .* Phisum_n * (1 + Phisum)^(f - 1)),
         vec(L0fK .* Phisum_n * ((1 + Phisum)^(f - 1) - 1)),
     )
     return w
 end
+
+
+function polyfc(args...)
+    ansType = promote_type(typeof(args[1]), typeof(args[2]), typeof(args[3]), eltype(args[4]), eltype(args[5]))
+    f! = (F, x) -> F .= args[4] .- polyfc_Req(x, args...).Rbound_n .- x
+    Req = rootSolve(f!, convert(Vector{ansType}, args[4]))
+    return polyfc_Req(Req, args...)
+end
+
 
 polyfcm = (Kₓ, f, Rtot, IgG, Kav) -> polyfc(sum(IgG) / f, Kₓ, f, Rtot, IgG ./ sum(IgG), Kav)
